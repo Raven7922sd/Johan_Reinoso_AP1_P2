a27e99f847d00b27584b43b852b38f59ad6dab99
Ravensd7922 <johan_reinoso@ucne.edu.do>
2025-07-07 18:53:31 -0400

    Contexto creado e injectado

---
+++ Johan_Reinoso_AP1_P2/Components/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Johan_Reinoso_AP1_P2.Components.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Johan_Reinoso_AP1_P2.Components.DAL;
+
+public class Contexto: DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options){}
+    public DbSet<ModeloParcial> Modelos { get; set; }
+}
