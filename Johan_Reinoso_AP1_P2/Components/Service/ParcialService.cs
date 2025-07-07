93364bf48301f3b6d7f0e0df0ac80feb32bf6dee
Ravensd7922 <johan_reinoso@ucne.edu.do>
2025-07-07 18:58:02 -0400

    Modelo vacío de service creado

---
+++ Johan_Reinoso_AP1_P2/Components/Service/ParcialService.cs
@@ -0,0 +1,8 @@
+﻿using Johan_Reinoso_AP1_P2.Components.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Johan_Reinoso_AP1_P2.Components.Service;
+
+public class ParcialService(IDbContextFactory<Contexto> DbFactory)
+{
+}
