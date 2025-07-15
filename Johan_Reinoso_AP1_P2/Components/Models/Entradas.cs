6b1cf3e9ce63d970a2dfa2df67e0a8f85f5ce28d
Ravensd7922 <johan_reinoso@ucne.edu.do>
2025-07-14 21:17:30 -0400

    Modelo entradas terminado

---
+++ Johan_Reinoso_AP1_P2/Components/Models/Entradas.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Johan_Reinoso_AP1_P2.Components.Models;
+
+public class Entradas
+{
+    [Key]
+    public int EntradasId { get; set; }
+
+    [Required(ErrorMessage = "Campo requerido")]
+    [StringLength(100, ErrorMessage = "Máximo 100 caracteres.")]
+    public string Concepto { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Campo requerido")]
+    public decimal PesoTotal { get; set; }
+
+    public int IdProducido { get; set; }
+
+    [Required(ErrorMessage = "Campo requerido")]
+    [Range(1, 999, ErrorMessage = "El valor debe ser mayor que cero.")]
+    public decimal CantidadProducida { get; set; }
+
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [InverseProperty("Entrada")]
+    public virtual ICollection<EntradasDetalles> EntradasDetalles { get; set; } = new List<EntradasDetalles>();
+}
