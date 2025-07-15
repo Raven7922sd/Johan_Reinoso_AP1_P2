c6542ec9bac40aafc64f659ea991071578bc3e55
Ravensd7922 <johan_reinoso@ucne.edu.do>
2025-07-14 21:16:55 -0400

    Contexto terminado

--- Johan_Reinoso_AP1_P2/Components/DAL/Contexto.cs
+++ Johan_Reinoso_AP1_P2/Components/DAL/Contexto.cs
@@ -3,8 +3,68 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Johan_Reinoso_AP1_P2.Components.DAL;
 
-public class Contexto: DbContext
+public class Contexto : DbContext
 {
-    public Contexto(DbContextOptions<Contexto> options) : base(options){}
-    public DbSet<ModeloParcial> Modelos { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Productos> productos { get; set; } 
+                                                   
+    public DbSet<Entradas> entradas { get; set; }
+    public DbSet<EntradasDetalles> entradasDetalles { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Productos>().HasData(
+            new Productos 
+            {
+                ProductoId = 1,
+                Descripcion = "Maní",
+                Peso = 0,
+                Existencia = 100,
+                EsCompuesto = false
+            },
+            new Productos
+            {
+                ProductoId = 2,
+                Descripcion = "Pistachos",
+                Peso = 0,
+                Existencia = 100,
+                EsCompuesto = false
+            },
+            new Productos
+            {
+                ProductoId = 3,
+                Descripcion = "Almendras",
+                Peso = 0,
+                Existencia = 100,
+                EsCompuesto = false
+            },
+            new Productos
+            {
+                ProductoId = 4,
+                Descripcion = "Frutos Mixtos 200gr",
+                Peso = 200.00m,
+                Existencia = 0,
+                EsCompuesto = true
+            },
+            new Productos
+            {
+                ProductoId = 5,
+                Descripcion = "Frutos Mixtos 400gr",
+                Peso = 400.00m,
+                Existencia = 0,
+                EsCompuesto = true
+            },
+            new Productos
+            {
+                ProductoId = 6,
+                Descripcion = "Frutos Mixtos 600gr",
+                Peso = 600.00m,
+                Existencia = 0,
+                EsCompuesto = true
+            }
+        );
+      
+    }
 }
