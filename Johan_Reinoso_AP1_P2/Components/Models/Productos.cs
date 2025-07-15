ffaef3efd3d551564b038bf6b360f77b4399a64f
Ravensd7922 <johan_reinoso@ucne.edu.do>
2025-07-14 21:17:59 -0400

    productos modelo terminado

---
+++ Johan_Reinoso_AP1_P2/Components/Models/Productos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Johan_Reinoso_AP1_P2.Components.Models;
+
+public class Productos
+{
+    [Key]   
+    public int ProductoId { get; set; }
+    
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [StringLength(100, ErrorMessage = "Máximo 100 caracteres.")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    public decimal Peso { get; set; }
+
+    public int Existencia { get; set; }
+
+    public bool EsCompuesto { get; set; } = false;
+}
