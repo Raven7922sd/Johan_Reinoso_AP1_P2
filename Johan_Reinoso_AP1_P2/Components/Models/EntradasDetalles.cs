fea75ab0f83afea186ee7d843561e7bae723f1ab
Ravensd7922 <johan_reinoso@ucne.edu.do>
2025-07-14 21:17:44 -0400

    Entradas detalles modelo terminado

---
+++ Johan_Reinoso_AP1_P2/Components/Models/EntradasDetalles.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Johan_Reinoso_AP1_P2.Components.Models;
+
+public class EntradasDetalles
+{
+    [Key]
+    public int EntradasDetallesId { get; set; }
+
+    public int ProductoId { get; set; }
+    [ForeignKey("ProductoId")]
+    public Productos Producto { get; set; } = null!;
+
+    public int EntradasId { get; set; }
+    [ForeignKey("EntradasId")]
+    public Entradas Entrada { get; set; } = null!;
+
+    public int Cantidad { get; set; }
+}
